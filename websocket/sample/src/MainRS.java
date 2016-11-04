d6373770f430ad988a40f4c9b9cca5aa2fa40d4b
Raphael Jolly <rjolly@users.sourceforge.net>
2016-11-04 12:28:21 +0100

    empty

--- websocket/sample/src/MainRS.java
+++ websocket/sample/src/MainRS.java
@@ -5,7 +5,10 @@ public class MainRS {
 	public static void main(final String[] args) throws Exception {
 		final RemoteFactory factory = RemoteFactory.apply("ws://localhost:8080/websockets/mediator", new com.sun.security.auth.callback.TextCallbackHandler());
 		final Remote<Object> obj = factory.lookup("obj");
-		final Remote<String> str = obj.map(a -> a.toString());
+		final Remote<String> str = obj.map(a -> {
+			System.getProperty("test");
+			return a.toString();
+		});
 		System.out.println(str);
 		System.out.println(str.get());
 	}
